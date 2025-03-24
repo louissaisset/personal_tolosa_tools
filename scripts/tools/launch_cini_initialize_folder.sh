46d6c4beb6006eda05470c92e0af6ce0356a5ea4
Louis <louis.saisset@shom.fr>
2025-03-24 09:46:30 +0100

    amelioration des verifications

--- scripts/tools/launch_cini_initialize_folder.sh
+++ scripts/tools/launch_cini_initialize_folder.sh
@@ -4,16 +4,16 @@ echo "Beginning the initialisation using 'cini'..."
 
 # Définir le chemin vers le répertoire cini
 set path_cini = ~/SOFTS/config_prep_tools/cini
-echo -e "       \e[32mOK:\e[0m Assumed path to cini: $path_cini"
+echo "       \e[32mOK:\e[0m Assumed path to cini: $path_cini"
 
 # Charger le module nécessaire
 source /usr/share/Modules/3.2.10/init/csh
 module load intel-fc-20/19.1.3
-echo -e "       \e[32mOK:\e[0m Loaded modules: intel-fc-20/19.1.3"
+echo "       \e[32mOK:\e[0m Loaded modules: intel-fc-20/19.1.3"
 
 # Sauvegarder le chemin de départ
 set start_path = $cwd
-echo -e "       \e[32mOK:\e[0m Selected folder: $start_path"
+echo "       \e[32mOK:\e[0m Selected folder: $start_path"
 
 # Vérifier l'existence d'un fichier .msh dans le dossier
 if (! `ls *.msh >& /dev/null; echo $status`) then
@@ -21,7 +21,7 @@ if (! `ls *.msh >& /dev/null; echo $status`) then
     # Ajout d'un warning Si de multiples fichiers .msh existent dans le dossier
     set txtCount = `find . -maxdepth 1 -name "*.msh" -type f | wc -l`
     if ($txtCount > 1) then
-        echo -e "  \e[33mWARNING:\e[0m Multiple .msh files found"
+        echo "  \e[33mWARNING:\e[0m Multiple .msh files found"
     endif
     
     # Utilisation de find pour récupérer seulement le premier fichier .msh
@@ -32,18 +32,18 @@ if (! `ls *.msh >& /dev/null; echo $status`) then
         
         # Récupération du chemin absolu
         set absolutePathmsh = `realpath "$firstTxtFile"`
-        echo -e "       \e[32mOK:\e[0m Found .msh file: $absolutePathmsh"
+        echo "       \e[32mOK:\e[0m Found .msh file: $absolutePathmsh"
         
         # Création du lien symbolique
         ln -sf $absolutePathmsh $path_cini/input.msh
-        echo -e "       \e[32mOK:\e[0m Added .msh file symbolic link to $path_cini/input.msh "
+        echo "       \e[32mOK:\e[0m Added .msh file symbolic link to $path_cini/input.msh "
 
     else
-        echo -e "    \e[31mERROR:\e[0m No .msh files found in the current directory."
+        echo "    \e[31mERROR:\e[0m No .msh files found in the current directory."
         exit 1
     endif
 else
-    echo -e "    \e[31mERROR:\e[0m No .msh files found in the current directory."
+    echo "    \e[31mERROR:\e[0m No .msh files found in the current directory."
     exit 1
 endif
 
@@ -53,14 +53,14 @@ if (! `ls regional.depth-ele.a >& /dev/null; echo $status`) then
 
     # Récupération du chemin absolu
     set absolutePathregional = `realpath ./regional.depth-ele.a`
-    echo -e "       \e[32mOK:\e[0m Found regional.depth-ele.a: $absolutePathregional"
+    echo "       \e[32mOK:\e[0m Found regional.depth-ele.a: $absolutePathregional"
     
     # Création du lien symbolique
     ln -sf ${start_path}/$f/regional.depth-ele.a $path_cini/regional.depth.a
-    echo -e "       \e[32mOK:\e[0m Added regional.depth-ele.a file symbolic link to $path_cini/input.msh "
+    echo "       \e[32mOK:\e[0m Added regional.depth-ele.a file symbolic link to $path_cini/input.msh "
 
 else
-    echo -e "    \e[31mERROR:\e[0m No regional.depth-ele.a files found in the current directory."
+    echo "    \e[31mERROR:\e[0m No regional.depth-ele.a files found in the current directory."
     exit 1
 endif
 
